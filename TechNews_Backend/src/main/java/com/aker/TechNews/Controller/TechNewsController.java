bf6213ca7c8ced0ecd6df160c604434818a60442
aker2302 <abdellah.kerbal@gmail.com>
2024-06-11 23:26:08 +0200

    Creation of controller class that contains the three endpoints

--- TechNews_Backend/src/main/java/com/aker/TechNews/Controller/TechNewsController.java
+++ TechNews_Backend/src/main/java/com/aker/TechNews/Controller/TechNewsController.java
@@ -1,5 +1,78 @@
 package com.aker.TechNews.Controller;
 
+import com.aker.TechNews.Service.ArticleService;
+import com.aker.TechNews.entity.Article;
+import com.aker.TechNews.response.ApiResponseHandler;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/news")
 public class TechNewsController {
+
+    private static final Logger log = LoggerFactory.getLogger(TechNewsController.class);
+    @Autowired
+    private ArticleService articleService;
+
+    @RateLimiter(name = "TechNewsLimiter", fallbackMethod = "getArticlesFallbackMethod")
+    @GetMapping("/articles/pagination")
+    public ResponseEntity<Object> fetchAllArticlesWithPagination(@RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "10") int size){
+        if(size>10)
+            return ResponseEntity.status(406).body("You cannot fetch more than 10 articles  per call");
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Article> articles = articleService.getArticlesByPage(pageable);
+
+        return ApiResponseHandler.handlePageableApiResponse(articles.getNumber(), articles.getTotalPages(), HttpStatus.OK, articles.getContent(), articles.getTotalElements());
+    }
+
+    @RateLimiter(name = "TechNewsLimiter", fallbackMethod = "getArticlesFallbackMethod")
+    @GetMapping("/articles/search")
+    public ResponseEntity<Object> fetchArticlesBySearch(@RequestParam(defaultValue = "0") int page,
+                                                        @RequestParam(defaultValue = "10") int size,
+                                                        @RequestParam List<String> words){
+        if(size>10)
+            return ResponseEntity.status(406).body("You cannot fetch more than 10 articles  per call");
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Article> articles = articleService.getArticlesBySearch(pageable, words);
+
+        return ApiResponseHandler.handlePageableApiResponse(articles.getNumber(), articles.getTotalPages(), HttpStatus.OK, articles.getContent(), articles.getTotalElements());
+    }
+
+    @RateLimiter(name = "TechNewsLimiter", fallbackMethod = "getArticlesFallbackMethod")
+    @GetMapping("/articles/sorting")
+    public ResponseEntity<Object> fetchArticlesBySoring(@RequestParam(defaultValue = "0") int page,
+                                                        @RequestParam(defaultValue = "10") int size,
+                                                        @RequestParam  String[] sort){
+        if(size>10)
+            return ResponseEntity.status(406).body("You cannot fetch more than 10 articles per call");
+
+        Sort.Direction direction = Sort.Direction.fromString(sort[1]);
+        Sort sortBy = Sort.by(direction, sort[0]);
+        Pageable pageable = PageRequest.of(page, size, sortBy);
+        Page<Article> articles = articleService.getArticlesByPage(pageable);
+
+        return ApiResponseHandler.handlePageableApiResponse(articles.getNumber(), articles.getTotalPages(), HttpStatus.OK, articles.getContent(), articles.getTotalElements());
+    }
+
+    public ResponseEntity<Object> getArticlesFallbackMethod(RequestNotPermitted requestNotPermitted){
+        log.info("Fallback method invoked");
+        log.info("Request not permitted : {}", requestNotPermitted.getMessage());
+        return ResponseEntity.status(406).body("You Exceeded number of request available in 15min");
+    }
+
 }
